908de99b2333e91bbadecd9909c121fd503dd186
Mehedi Hasan <mehedi705bd@gmail.com>
2022-01-05 08:25:48 +0600

    Creating initial Project Structure [V:0.0.2]

--- the_app/views.py
+++ the_app/views.py
@@ -10,7 +10,7 @@ def signin(request):
     return HttpResponse("This is the signin page!")
 
 def home(request):
-    return HttpResponse("This is the home page!")
+    return render(request, 'index.html')
 
 def Profile(request):
     return HttpResponse("This is the profile page!")
