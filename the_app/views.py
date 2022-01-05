98f09d854d8d73fcb1a83d6e7da13fac3608b91b
Mehedi Hasan <mehedi705bd@gmail.com>
2022-01-05 17:51:33 +0600

    Functional registration and login system [V:0.1.2]

--- the_app/views.py
+++ the_app/views.py
@@ -1,22 +1,59 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def signup(request):
-    return HttpResponse("This is the signup page!")
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['pass1']
+
+        new_user = User.objects.create_user(username, email, password)
+        new_user.save()
+
+        user = authenticate(username = username, password = password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse("Your login complete")
+
+
+    return render(request, 'signup.html')
 
 def signin(request):
-    return HttpResponse("This is the signin page!")
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username = username, password = password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse('your login complete')
+
+
+    return render(request, 'signin.html')
 
 def home(request):
-    return render(request, 'welcome.html')
+    if request.user.is_authenticated:
+        return HttpResponse("your are authenticated!")
+    else:    
+        return render(request, 'welcome.html')
 
 def Profile(request):
     return HttpResponse("This is the profile page!")
 
 def signout(request):
-    return HttpResponse("This is the signout page!")
+    if request.user.is_authenticated:
+        logout(request)
+    
+    return HttpResponse("Signout properly")
 
 def dev_info(request):
     return render(request, 'dev_info.html')
